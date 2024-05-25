da2e1b393340a69fe8832609009ee8ff51ff4ab8
Arnaud P <arnaud@xpressive.io>
2024-05-26 00:15:10 +0200

    Products seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -21,5 +22,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         User::factory(5)->create();
+
+        Product::factory(30)->create();
     }
 }
