9527a10fa2968cd5b4c2ca035117e28a7ae26386
Arnaud P <arnaud@xpressive.io>
2024-05-25 23:51:09 +0200

    First draft Product model

---
+++ src/Catalog/Domain/ProductInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xp\StockManager\Catalog\Domain;
+
+interface ProductInterface
+{
+    public const float VAT_20 = 20.0;
+    public const float VAT_10 = 10.0;
+    public const float VAT_55 = 5.5;
+}
