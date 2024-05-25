976bc77a268043ce2cd1df5fc80de0edeb6a68b7
Arnaud P <arnaud@xpressive.io>
2024-05-26 00:40:47 +0200

    Product creation route

---
+++ app/Http/Controllers/API/StoreController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+abstract class StoreController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $validator = $this->getValidator($request);
+
+        if ($validator->fails()) {
+            return response()->json(['message' => 'Invalid data sent', 'errors' => $validator->errors()], Response::HTTP_BAD_REQUEST);
+        }
+
+        $resourceId = $this->execute($request, $validator->validated());
+
+        return response()->json([
+            'id' => $resourceId,
+        ], Response::HTTP_CREATED);
+    }
+
+    abstract protected function getValidator(Request $request): ?Validator;
+
+    abstract protected function execute(Request $request, array $validated = []): string;
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,24 @@
 <?php
 
 use App\Http\Controllers\API as APIControllers;
+use App\Http\Middleware\Auth as AuthMiddleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
     Route::get('/', fn() => response()->json(['version' => config('app.api.version'), 'status' => 'OK']));
 
-    Route::middleware(\App\Http\Middleware\Auth\EnsureJWTAuthentication::class)->group(function () {
+    Route::middleware(AuthMiddleWare\EnsureJWTAuthentication::class)->group(function () {
         Route::get('/me', fn(Request $request) => $request->user());
     });
 
     Route::prefix('product')->group(function () {
         Route::get('/', APIControllers\Product\ListProductController::class)->name('api_product_list');
         Route::get('/{product}', APIControllers\Product\ReadProductController::class)->name('api_product_read');
+
+        Route::middleware([AuthMiddleWare\EnsureJWTAuthentication::class])->group(function () {
+            Route::post('/', APIControllers\Product\StoreProductController::class)->name('api_product_store');
+        });
     });
 });
 
